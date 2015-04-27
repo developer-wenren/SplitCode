c1726445c4208969d2c69a6f645ad1c43825e477
wrcj <wrcj12138@163.com>
2015-04-27 22:31:35 +0800

    Initial Commit

---
+++ SplitCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SplitCode
+//
+//  Created by zjsruxxxy3 on 15/4/27.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
